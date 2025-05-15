0baabd6d414d4883beafc3b7092911d3be2a2114
arpit sharma <arpit.it123@gmail.com>
2025-05-15 23:14:45 +0530

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,34 +9,30 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Aviz Networks`,
+    description: `Next-gen networking solutions`,
+    author: `@AvizNetworks`,
+    siteUrl: `https://aviznetworks.com`,
   },
   plugins: [
-    `gatsby-plugin-image`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-graphql",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        typeName: "PAYLOAD",
+        fieldName: "payload",
+        url: "https://your-payload-cms-endpoint/graphql",
+        // Replace "https://your-payload-cms-endpoint/graphql" with your Payload CMS GraphQL endpoint.
       },
     },
   ],
