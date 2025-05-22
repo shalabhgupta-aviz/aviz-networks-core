6f8f6cfe8b0668c303e6dcae9288e28cd8363451
arpit sharma <arpit.it123@gmail.com>
2025-05-22 21:21:15 +0530

    updates

--- src/pages/calculate-your-savings-sonic-ones-network-observability-aviz-networks.js
+++ src/pages/calculate-your-savings-sonic-ones-network-observability-aviz-networks.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 import TCOCalculatorHome from "../components/tco-calculator/TCOCalculatorHome"
-
+import Seo from "../components/seo"
 function TCOHome() {
   return (
     <Layout>
+      <Seo title="Calculate Your Savings" />
       <TCOCalculatorHome />
     </Layout>
   )
---
+++ src/pages/resources/blogs/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../../../components/layout"
+import Seo from "../../../components/seo"
+
+const BlogsPage = () => (
+  <Layout>
+    <Seo title="Blogs" />
+    <h1>Blogs</h1>
+  </Layout>
+)
+
+export default BlogsPage
