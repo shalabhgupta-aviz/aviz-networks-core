d8df69f876bdba708839e52625670a2382c36e4e
arpit sharma <arpit.it123@gmail.com>
2025-05-23 08:33:51 +0530

    updates

--- src/components/resources/blog/Card.jsx
+++ src/components/resources/blog/Card.jsx
@@ -1,8 +1,20 @@
 import React from "react"
 import * as styles from "./Card.module.css"
 
-function Card() {
-  return <div>Card</div>
+function Card({ title, description, date, image }) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardImage}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={styles.cardContent}>
+        <div className={styles.cardDate}>{date}</div>
+        <div className={styles.cardTitle}>{title}</div>
+        <div className={styles.cardDescription}>{description}</div>
+        <div className={styles.cardReadMore}>Read More</div>
+      </div>
+    </div>
+  )
 }
 
 export default Card
--- src/components/resources/blog/List.jsx
+++ src/components/resources/blog/List.jsx
@@ -3,9 +3,35 @@ import * as styles from "./List.module.css"
 import Card from "./Card"
 
 function List() {
+  const dummyData = [
+    {
+      title: "Blog 1",
+      description: "Description 1",
+      date: "2025-05-23",
+      image:
+        "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
+    },
+    {
+      title: "Blog 2",
+      description: "Description 2",
+      date: "2025-05-23",
+      image:
+        "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
+    },
+    {
+      title: "Blog 3",
+      description: "Description 3",
+      date: "2025-05-23",
+      image:
+        "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
+    },
+  ]
+
   return (
     <div className={styles.list}>
-      <Card />
+      {dummyData.map(item => (
+        <Card key={item.title} {...item} />
+      ))}
     </div>
   )
 }
