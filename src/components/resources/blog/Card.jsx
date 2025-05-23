52f86d3520431850ccb393056fa000e4505f575c
arpit sharma <arpit.it123@gmail.com>
2025-05-23 08:52:41 +0530

    updates

--- src/components/resources/blog/Card.jsx
+++ src/components/resources/blog/Card.jsx
@@ -1,17 +1,31 @@
 import React from "react"
 import * as styles from "./Card.module.css"
+import { Link } from "gatsby"
 
-function Card({ title, description, date, image }) {
+function Card(post) {
+  console.log(post)
+  const { title, description, date, image, slug } = post
   return (
     <div className={styles.card}>
       <div className={styles.cardImage}>
         <img src={image} alt={title} />
       </div>
       <div className={styles.cardContent}>
-        <div className={styles.cardDate}>{date}</div>
+        <div className={styles.cardDate}>
+          {new Date(date).toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
+        </div>
         <div className={styles.cardTitle}>{title}</div>
-        <div className={styles.cardDescription}>{description}</div>
-        <div className={styles.cardReadMore}>Read More</div>
+        <div
+          className={styles.cardDescription}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+        <div className={styles.cardReadMore}>
+          <Link to={`/resources/blogs/${slug}`}>Read More</Link>
+        </div>
       </div>
     </div>
   )
--- src/components/resources/blog/List.jsx
+++ src/components/resources/blog/List.jsx
@@ -1,36 +1,52 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import * as styles from "./List.module.css"
 import Card from "./Card"
 
 function List() {
-  const dummyData = [
-    {
-      title: "Blog 1",
-      description: "Description 1",
-      date: "2025-05-23",
-      image:
-        "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
-    },
-    {
-      title: "Blog 2",
-      description: "Description 2",
-      date: "2025-05-23",
-      image:
-        "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
-    },
-    {
-      title: "Blog 3",
-      description: "Description 3",
-      date: "2025-05-23",
-      image:
-        "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
-    },
-  ]
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          "https://staging.aviznetworks.com/wp-json/wp/v2/posts"
+        )
+        const formattedPosts = response.data.map(post => ({
+          title: post.title.rendered,
+          description: post.excerpt.rendered,
+          date: post.date,
+          image:
+            post._embedded?.["wp:featuredmedia"]?.[0]?.source_url ||
+            "https://aviznetworks.com/wp-content/uploads/2025/05/ip-ones-3-BLOG-Banner.png",
+          slug: post.slug,
+        }))
+        setPosts(formattedPosts)
+        setLoading(false)
+      } catch (err) {
+        setError("Failed to fetch blog posts")
+        setLoading(false)
+        console.error("Error fetching posts:", err)
+      }
+    }
+
+    fetchPosts()
+  }, [])
+
+  if (loading) {
+    return <div className={styles.loading}>Loading...</div>
+  }
+
+  if (error) {
+    return <div className={styles.error}>{error}</div>
+  }
 
   return (
     <div className={styles.list}>
-      {dummyData.map(item => (
-        <Card key={item.title} {...item} />
+      {posts.map(post => (
+        <Card key={post.title} {...post} />
       ))}
     </div>
   )
